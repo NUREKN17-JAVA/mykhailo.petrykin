43d9d6efef9903aeb74e7205c31c3529c7591baf
misha-petrykin <mykhailo.petrykin@nure.ua>
2019-12-17 15:22:09 +0200

    Composing a message in response to a request to search for user records

--- usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/agent/RequestServer.java
+++ usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/agent/RequestServer.java
@@ -1,9 +1,15 @@
 package ua.nure.cs.petrykin.usermanagement.agent;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import ua.nure.cs.petrykin.usermanagement.db.DaoFactory;
+import ua.nure.cs.petrykin.usermanagement.db.DatabaseException;
+import ua.nure.cs.petrykin.usermanagement.domain.User;
 
 public class RequestServer extends CyclicBehaviour {
 
@@ -29,8 +35,30 @@ public class RequestServer extends CyclicBehaviour {
 	}
 
 	private ACLMessage createReply(ACLMessage message) {
-		// TODO Auto-generated method stub
-		return null;
+		ACLMessage reply = message.createReply();
+		
+		String content = message.getContent();
+		StringTokenizer tokenizer = new StringTokenizer(content, ",");
+		if(tokenizer.countTokens()==2) {
+			String firstName = tokenizer.nextToken();
+			String lastName = tokenizer.nextToken();
+			Collection users = null;
+			try {
+				users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			} catch(DatabaseException e) {
+				e.printStackTrace();
+				users = new ArrayList(0);
+			}
+			StringBuffer buffer = new StringBuffer();
+			for(Iterator it = users.iterator(); it.hasNext();) {
+				User user = (User) it.next();
+				buffer.append(user.getId()).append(",");
+				buffer.append(user.getFirstName()).append(",");
+				buffer.append(user.getLastName()).append(",");
+			}
+			reply.setContent(buffer.toString());
+		}
+		return reply;
 	}
 
 }
