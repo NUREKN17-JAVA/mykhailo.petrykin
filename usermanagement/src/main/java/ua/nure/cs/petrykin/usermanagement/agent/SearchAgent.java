1fe047a6ba8e2b8f4530ced2aae2f9245becbb9c
misha-petrykin <mykhailo.petrykin@nure.ua>
2019-12-17 15:36:38 +0200

    Adding message processing agent behavior and registering with jades Yellow Pages service

--- usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/agent/SearchAgent.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import ua.nure.cs.petrykin.usermanagement.db.DaoFactory;
 import ua.nure.cs.petrykin.usermanagement.db.DatabaseException;
 import ua.nure.cs.petrykin.usermanagement.domain.User;
@@ -18,11 +22,31 @@ public class SearchAgent extends Agent {
 	protected void setup() {
 		super.setup();
 		System.out.println(getAID().getName()+" started");
+		
+		DFAgentDescription description = new DFAgentDescription();
+		description.setName(getAID());
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName("JADE-searching");
+		serviceDescription.setType("searching");
+		description.addServices(serviceDescription);
+		try {
+			DFService.register(this,description);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		
+		addBehaviour(new RequestServer());
 	}
 
 	@Override
 	protected void takeDown() {
 		System.out.println(getAID().getName()+" terminated.");
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		
 		super.takeDown();
 	}
 	
