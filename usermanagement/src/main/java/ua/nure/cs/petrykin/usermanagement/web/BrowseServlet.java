ed98fe566f56709d28981015ec801abfb4eb85d7
misha-petrykin <mykhailo.petrykin@nure.ua>
2019-12-17 12:29:18 +0200

    Client side integration with final setup of the web project

--- usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/cs/petrykin/usermanagement/web/BrowseServlet.java
@@ -58,8 +58,7 @@ public class BrowseServlet extends HttpServlet {
 	}
 
 	private void add(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		req.getRequestDispatcher("/add").forward(req,resp);
 	}
 
 	private void browse(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException  {
